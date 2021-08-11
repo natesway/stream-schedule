00b15717e511f61bdfc32a7b90ca5848504d0c6b
natesway <64743334+natesway@users.noreply.github.com>
2021-08-11 15:28:48 -0400

    Update newuser.js

--- scripts/newuser.js
+++ scripts/newuser.js
@@ -2,12 +2,12 @@ function newuser(form) {
 	registercode = new Array("907362", "432176");/* 1st code for admin, 2nd one for members code*/
 
 	if(form.code.value == registercode[0]) {
-		alert("Please contact your Administrator to recieve your admin username and password. The member site username is 'members' and password is 'ewbuccclub'")/* When you enter admin code */
+		alert("Please contact your Administrator to recieve your admin username and password. The member site username is 'betatester' and password is 'BETA123'")/* When you enter admin code */
 	}
 	else if(form.code.value == registercode[1]) {
-		alert("Username - 'members', password - 'ewbuccclub'")/* Member username and password*/
+		alert("Hey Beta Testers! Username - 'betatester', password - 'BETA123'"))/* Member username and password*/
 	}
 	else {
-		alert("Incorrect code. Please re-try or contact Michael for assistance.")/* No such user */
+		alert("Incorrect code. Please re-try or as natesway4 for the beta code again!.")/* No such user */
 	}
 }
