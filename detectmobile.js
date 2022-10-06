5dec22f16ad407fc925029591e71f04491ea24d2
natesway <64743334+natesway@users.noreply.github.com>
2022-10-06 18:09:00 -0400

    Update detectmobile.js

--- detectmobile.js
+++ detectmobile.js
@@ -1,7 +1,7 @@
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile) {
  // Simulate a mouse click:
-window.location.href = "https://natesway.github.io/mobile.html";
+window.location.href = "https://natesway.github.io/mobile";
 
-window.location.replace("https://natesway.github.io/mobile.html");
+window.location.replace("https://natesway.github.io/mobile");
 }
